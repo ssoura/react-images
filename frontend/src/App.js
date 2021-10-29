c9a83e97e9fd110faa2ba2bbf276f44de097f9ec
Sora <19mca8281@cuchd.in>
2021-10-29 16:41:32 +0530

    Added Unsplash Api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import { useState } from 'react';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
   const[word, setWord] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res) => res.json())
+    .then((data) => { 
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
+  
   return (
     <div className="App">
       <Header title="Images Gallery" word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
